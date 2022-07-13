bfd56f347d2dd66834980e9d38a65f354d4a09c9
Aaron <66818730@qq.com>
2022-07-13 15:24:35 +0800

    初始化

--- packages/m1/src/index.ts
+++ packages/m1/src/index.ts
@@ -7,9 +7,9 @@ import { m2 } from 'kulem2'
  * ```js
  * kulem1()
  * ```
- * @param v - paa11
+ * @param v - paa1122
  */
 export const kulem1 = (v: string) => {
   m2()
-  console.log('kulem1 runing2223', v)
+  console.log('kulem1 runing222322', v)
 }
